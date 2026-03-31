55e2f9190d02de7fa1f635070e8c47c327fb11d4
motasemgh83 <Motasemghanem83@gmail.com>
2026-03-31 13:20:30 +0300

    Update config.js

--- apps/api/src/config.js
+++ apps/api/src/config.js
@@ -1,2 +1,7 @@
 require('dotenv').config();
-module.exports = { port: Number(process.env.API_PORT || 4000), jwtSecret: process.env.JWT_SECRET || 'dev-secret', databaseUrl: process.env.DATABASE_URL };
+
+module.exports = {
+  port: Number(process.env.PORT || process.env.API_PORT || 4000),
+  jwtSecret: process.env.JWT_SECRET || 'dev-secret',
+  databaseUrl: process.env.DATABASE_URL
+};
