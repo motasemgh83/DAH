bedc316dcecd62d710ffc421b528073b96bea551
motasemgh83 <Motasemghanem83@gmail.com>
2026-03-31 12:17:35 +0300

    Update assetController.js

--- apps/api/src/controllers/assetController.js
+++ apps/api/src/controllers/assetController.js
@@ -1,3 +1,5 @@
+const prisma = require("../prisma");
+
 async function listAssets(req, res, next) {
   try {
     const data = await prisma.asset.findMany({
@@ -30,3 +32,46 @@ async function listAssets(req, res, next) {
     next(error);
   }
 }
+
+async function listLocations(req, res, next) {
+  try {
+    const data = await prisma.unit.findMany({
+      include: {
+        branch: {
+          include: {
+            property: {
+              include: {
+                client: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    res.json({ data });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function listPreventivePlans(req, res, next) {
+  try {
+    const data = await prisma.preventivePlan.findMany({
+      include: {
+        asset: true,
+        checklistItems: true,
+      },
+    });
+
+    res.json({ data });
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = {
+  listAssets,
+  listLocations,
+  listPreventivePlans,
+};
